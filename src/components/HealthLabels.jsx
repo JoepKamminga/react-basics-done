426224d2fd38178132f89c056386b557ee7b7689
JoepKamminga <joep_k@hotmail.nl>
2025-02-25 09:44:16 +0100

    Update HealthLabels.jsx
    deleted a console.log()

--- src/components/HealthLabels.jsx
+++ src/components/HealthLabels.jsx
@@ -1,7 +1,7 @@
 import { Badge } from "@chakra-ui/react";
 
 export const HealthLabels = ({ recipe }) => {
-  //console.log(recipe.dietLabels);
+ 
   return recipe.healthLabels.map((healthLabels) => {
     return (
       <Badge marginRight="2" colorScheme="purple">
