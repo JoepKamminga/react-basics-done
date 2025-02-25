56c25b5c7ba4a3673a6826ba82c7f6d8d471581f
JoepKamminga <joep_k@hotmail.nl>
2025-02-25 09:43:34 +0100

    Update Cautions.jsx
    deleted a console.log()

--- src/components/Cautions.jsx
+++ src/components/Cautions.jsx
@@ -1,7 +1,6 @@
 import { Badge } from "@chakra-ui/react";
 
 export const Cautions = ({ recipe }) => {
-  //console.log(recipe.cautions);
   return recipe.cautions.map((cautions) => {
     return (
       <Badge marginRight="2" colorScheme="red">
